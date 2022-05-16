aab26a88f17f4487ccc57170c307eb7d36580ff3
nagar <nagar.2@iitj.ac.in>
2022-05-16 22:41:54 +0530

    changes made in about.js file

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -136,22 +136,19 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+            Hello!!! My name is Rohit Nagar, a final-year undergraduate student at Indian Institute of Technology Jodhpur. I have keen interest in building web apps including both frontend and backend. My Interest develope with HTML &amp; CSS! 
             </p>
 
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
+            //   <p>
+            //   Fast-forward to today, and I’ve had the privilege of working at{' '}
+            //   <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
+            //   <a href="https://starry.com/">a start-up</a>,{' '}
+            //   <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
+            //   <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
+            //   main focus these days is building accessible, inclusive products and digital
+            //   experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
+            //   clients.
+            // </p>
 
             <p>
               I also recently{' '}
